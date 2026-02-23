83123fa6c0dccaeacd45da845a941af8c08fb6a1
Bert <bertdec@hotmail.com>
2026-02-23 11:44:26 +0000

    install zhell and gnome-screenshot

--- basic-programs.sh
+++ basic-programs.sh
@@ -2,3 +2,6 @@ sudo snap install spotify
 sudo snap install --classic code
 sudo snap install slack
 sudo snap install htop
+sudo apt install gnome-screenshot
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
